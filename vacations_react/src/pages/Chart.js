eedaa905439617d08d91eedcead111acaf7e4477
Yoni <yoni29396@gmail.com>
2019-06-29 08:54:35 +0300

    chart update

--- vacations_react/src/pages/Chart.js
+++ vacations_react/src/pages/Chart.js
@@ -2,11 +2,25 @@ import React, { Component } from "react";
 import { BarChart, Bar, XAxis, YAxis,Tooltip, Legend, CartesianGrid } from 'recharts';
 import axios from 'axios'
 import {Link} from "react-router-dom";
+
+import "../styles/mainPageStyle.css" ;
+import {Background} from "../styles/Background.js"
+
 class Chart extends Component {
+    
     state = {
-        vacations : []
+        vacations : [],
+        w : window.innerWidth - (window.innerWidth/10),
+        h : window.innerHeight - (window.innerHeight/10)
+
     }
+    reportWindowSize = () => {
+        let h = window.innerHeight - (window.innerHeight/10);
+        let w = window.innerWidth - (window.innerWidth/10);
+        this.setState({h,w})
+      }
     componentDidMount = async () => {
+        window.onresize = this.reportWindowSize;
         let vacations = await axios.get('http://localhost:8080/getVacationChart')
         let chartVacations = vacations.data.map(vac => {
             let newVac = {
@@ -23,27 +37,40 @@ class Chart extends Component {
 
     render(){
         var centerCon = {
-            height: '100%',
+            height: '100vh',
             width:'100vw',
             display: 'flex',
             alignItems: 'center',
-            justifyContent: 'center'
+            justifyContent: 'center',
+            paddingRight:'80px',
+            paddingBottom:'70px'
         }
-        var w = window.innerWidth - (window.innerWidth/10);
-        var h = window.innerHeight - (window.innerHeight/10);
-
+        var background = new Background('#b2cecf');
         return(
-            <div className='container' style={centerCon}>
-                <BarChart width={w} height={h} data={this.state.vacations}>
-                 <XAxis dataKey="name" stroke="#8884d8" />
-                    <YAxis />
-                    <Tooltip wrapperStyle={{ width: 100, backgroundColor: '#ccc' }} />
-                    <Legend width={100} wrapperStyle={{ top: 40, right: 20, backgroundColor: '#f5f5f5', border: '1px solid #d5d5d5', borderRadius: 3, lineHeight: '40px' }} />
-                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                    <Bar type="monotone" dataKey="Followers" fill="#8884d8" barSize={30} />
-                </BarChart>
-                <Link className='p-0 m-0' to="/"><button className='btn btn-outline-primary btn-block' style={{top:'92vh',width:'98vw'}}>Back</button></Link>
-          </div>
+            <div className="background p-0 m-0" style={background}>
+            <div className="container-fluid p-0 m-0" style={{height:'100vh',width:'100vw',textAlign:'center',}}>
+                <h1 className='chartTitle'>Followers status</h1>
+                <hr/>
+                <div className='container-fluid m-0' style={centerCon}>
+                    <BarChart width={this.state.w} height={this.state.h}  data={this.state.vacations}>
+                    <XAxis dataKey="name" stroke="#d02552" />
+                        <YAxis />
+                        <Tooltip wrapperStyle={{ width: 120, backgroundColor: '#ccc' , color:'#50d07d' }} />
+                        <Legend 
+                         width={150} 
+                         wrapperStyle={{ top: 40, right: 20, backgroundColor: '#f5f5f5', border: '1px solid #d02552', borderRadius: 3, lineHeight: '40px' }}
+                        />
+                        <CartesianGrid stroke="rgba(208, 37, 82,0.2)" strokeDasharray="5 5" />
+                        <Bar type="monotone" dataKey="Followers" fill="#d02552" barSize={40} />
+                    </BarChart>
+                </div>
+                <div className="row">
+                    <Link className='offset-xl-5 offset-lg-5 offset-md-4 offset-2' to="/" style={{position:'absolute',top:'94vh'}} >
+                        <button className='btn btn-outline-success btn-block' style={{width:'270px'}}>Back</button>
+                    </Link>
+                </div>
+            </div>
+            </div>
         )
     }
 }
--- vacations_react/src/styles/Background.js
+++ vacations_react/src/styles/Background.js
@@ -1,19 +1,3 @@
-// export var Body = {
-//     background: "url('https://images.unsplash.com/photo-1436491865332-7a61a109cc05?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80') no-repeat center center fixed",
-//     backgroundSize: 'cover',
-//     width: '100vw',
-//     height:'100vh',
-//     fontFamily: '"Numans" , sans - serif',
-
-// } 
-
-// export var mainBody = {
-//     background: 'url("/cloud.jpg")',
-//     backgroundSize: 'cover',
-//     width: '100%',
-//     height:'100%',
-//     fontFamily: '"Numans" , sans - serif',
-// } 
 export class  Background{
     constructor(background){
         this.background = background;
